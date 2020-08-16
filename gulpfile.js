3c3adc57463d712c83abb74d4c6b8bdf0d8e089a
Hung Cung Issac <cungthehung@gmail.com>
2020-08-16 23:39:45 +0700

    reconfig gulp for serve

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,40 @@
-var gulp        = require('gulp');
+var { src, dest, series, parallel, watch } = require('gulp');
 var browserSync = require('browser-sync').create();
-var reload      = browserSync.reload;
+var sass = require('gulp-sass');
+
+function buildScss() {
+  return src('scss/*.scss')
+              .pipe(sass())
+              .pipe(dest('dist'))
+              .pipe(browserSync.stream())
+}
+
+function moveIndex() {
+  return src('index.html')
+    .pipe(dest('dist'))
+}
 
 
 // Static server
-gulp.task('browser-sync', function() {
+function serve(done) {
     browserSync.init({
-        files: ["index/html"],
+        open: false,
+        files: ['*.html', '*.css'],
         server: {
-            baseDir: "./",
+            baseDir: "./dist",
             index: 'index.html'
         }
     });
 
-    gulp.watch("index.html").on("change", reload);
-});
+    done()
+};
+
+function watchFileChange() {
+  watch("scss/*.scss").on('change', buildScss)
+  watch("index.html").on('change', moveIndex)
+}
+exports.default = series(
+  parallel(moveIndex, buildScss),
+  serve,
+  watchFileChange
+)
